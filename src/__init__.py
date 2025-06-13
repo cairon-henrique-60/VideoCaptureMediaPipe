f203f133bf1a21a765e86d481a989ba686d7ea61
cairon-henrique-60 <caironhenrique60@gmail.com>
2025-06-13 08:23:51 -0300

    Feat. Create repository and published.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,13 @@
 from src.VideoCaptureFromMediaPipe import detect_faces
 
-# Chama a função de detecção de faces
-detect_faces()
+def main():
+    response = input("Would you like to start face detection? (y/n): ").strip().lower()
+    if response == 'y':
+        print("Face detection started. Press ESC to exit.")
+        detect_faces()
+    else:
+        print("Exiting the program. Goodbye!")
+
+
+if __name__ == "__main__":
+    main()
